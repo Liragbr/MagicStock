a2820bd476e54a446aba0ef23c70421a9e5d9d34
Felipe <felipekislyy@gmal.com>
2024-09-08 14:28:26 -0300

    testando banco de dados

--- server/index.js
+++ server/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
-const postgresql = express('db');
 
-const db = postgresql.createPool({
- 
-    host:"localhost",
-    user:"user_db_1725757023783_1zivs6g1lyj",
-    password:"<DlEh*YrKhMj)39c",
-    database:"db_1725757023783_1zivs6g1lyj",
-});
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// Configuração do Pool de Conexões com PostgreSQL
+const db = new Pool({
+  host: 'localhost',
+  user: 'postgres',
+  password: '15121608',
+  database: 'db_usuario',
+  port: 5432,
 });
 
+// Middleware para tratar JSON
+app.use(express.json());
 
-app.listen(3001, () => {
-  console.log('Hello World');
+// Rota POST para inserir um usuário específico no banco de dados
+app.post('/add-user', async (req, res) => {
+  try {
+    // Valores específicos que você deseja inserir
+    const query = "INSERT INTO usuarios(email, usuario, senha) VALUES ('fulanindital@gmail.com', 'fulanindital', '123456')";
+    
+    // Executar a consulta
+    await db.query(query);
+    res.status(201).send('Usuário inserido com sucesso!');
+  } catch (err) {
+    console.error('Erro ao inserir usuário:', err);
+    res.status(500).send('Erro ao inserir usuário');
+  }
+});
+
+// Iniciando o servidor Express na porta 3002
+app.listen(3002, () => {
+  console.log('Servidor rodando na porta 3002');
 });
