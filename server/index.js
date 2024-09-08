6c8f18adcb545713031cb3522afd2359238de365
Felipe <felipekislyy@gmal.com>
2024-09-07 22:22:26 -0300

    Testando conexão com o bd

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const postgresql = express('db');
 
+const db = postgresql.createPool({
+ 
+    host:"localhost",
+    user:"user_db_1725757023783_1zivs6g1lyj",
+    password:"<DlEh*YrKhMj)39c",
+    database:"db_1725757023783_1zivs6g1lyj",
+});
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
