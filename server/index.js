c9b005a6a0fc35a73b3ab71588be56becb4a4fee
rogs55 <bernardoramiro159@gmail.com>
2024-10-28 10:07:08 -0300

    conectando express

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const { Pool } = require("pg");
 const app = express();
+const port = 3000;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+const pool = new Pool({
+  user: "postgre",
+  host: "localhost",
+  database: "postgres",
+  password: "Rogs4455@",
+  port: 5432,
+});
 
-app.listen(3001, () => {
-  console.log('Hello World');
+app.get("/dados", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM sua_tabela");
+    res.json(result.rows); 
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao buscar dados");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
